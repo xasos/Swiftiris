a48e93a90a58878287969d4edd794b287c1d61c6
Niraj Pant <nirajpant7@gmail.com>
2014-10-31 21:41:09 -0500

    Add time element

---
+++ Swiftiris/Array2D.swift
@@ -0,0 +1,32 @@
+//
+//  Array2D.swift
+//  Swiftiris
+//
+//  Created by Niraj  on 10/31/14.
+//  Copyright (c) 2014 Niraj. All rights reserved.
+//
+
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+    
+}
--- Swiftiris/GameViewController.swift
+++ Swiftiris/GameViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure the view
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
 
     }
 
